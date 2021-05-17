db9e673981ba96b6eba9a886485957d9aa05e171
Apoorva <vermaapoorva0510@gmail.com>
2021-05-17 10:17:17 +0530

    op

--- static/js/addjs.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $("input[type='radio']").change(function() {
-        if ($(this).val() == "yes") {
-            $("#ifYes").show(800);
-        } else {
-            $("#ifYes").hide(800);
-        }
-    });
-});
